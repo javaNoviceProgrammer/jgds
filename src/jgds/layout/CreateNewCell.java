153a54ada9d4793169ccee3785a9c071e4dc9049
javaNoviceProgrammer <meisam@c-73-211-150-252.hsd1.il.comcast.net>
2018-10-14 09:49:59 -0400

    minor changes.

--- src/jgds/layout/CreateNewCell.java
+++ src/jgds/layout/CreateNewCell.java
@@ -1,5 +1,18 @@
 package jgds.layout;
 
+import jgds.elements.AbstractElement;
+
 public class CreateNewCell {
+
+	String cellName ;
+	AbstractElement[] elements ;
+
+	public CreateNewCell(
+			String cellName,
+			AbstractElement[] elements
+			) {
+		this.cellName = cellName ;
+		this.elements = elements ;
+	}
 
 }
