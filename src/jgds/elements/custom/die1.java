5a0ba338f72d6086c15179409f16ea65cd8e2d3e
javaNoviceProgrammer <40440007+javaNoviceProgrammer@users.noreply.github.com>
2018-10-17 11:12:56 -0500

    moving files.

--- src/jgds/elements/custom/die1.java
+++ src/jgds/elements/custom/die1.java
@@ -2,16 +2,18 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package jgds.elements;
+package jgds.elements.custom;
 
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import JGDS2.Array;
 import JGDS2.GDSWriter;
 import JGDS2.Lib;
 import JGDS2.Rect;
+import JGDS2.Ref;
 import JGDS2.Struct;
 
 /**
@@ -19,25 +21,32 @@ import JGDS2.Struct;
  * 
  * @author rob
  */
-public class unitCell {
+public class die1 {
 
     public static void main(String[] args) {
         try {
             FileOutputStream fileOUT;
-            File f = new File("unitCell.gds");
+            File f = new File("die1.gds");
             fileOUT = new FileOutputStream(f);
             DataOutputStream dO = new DataOutputStream(fileOUT);
             GDSWriter g = new GDSWriter(dO);
             Lib lib = new Lib();
             
             Struct topCell = new Struct("top") ;
-            topCell.add(new Rect(0.0, 0.0, 4e-3, 20e-3, 2));
-            topCell.add(new Rect(0.0, 44e-3, 4e-3, 44e-3+20e-3, 2));
-            topCell.add(new Rect(36e-3, 0.0, 36e-3+4e-3, 20e-3, 2));
-            topCell.add(new Rect(36e-3, 44e-3, 36e-3+4e-3, 44e-3+20e-3, 2));
-            topCell.add(new Rect(12e-3, 10e-3, 12e-3+4e-3, 10e-3+40e-3, 2));
-            topCell.add(new Rect(24e-3, 10e-3, 24e-3+4e-3, 10e-3+40e-3, 2));
+            Struct unitCell = new Struct("unitCell") ;
+            unitCell.add(new Rect(0.0, 0.0, 4e-3, 20e-3, 2));
+            unitCell.add(new Rect(0.0, 44e-3, 4e-3, 44e-3+20e-3, 2));
+            unitCell.add(new Rect(36e-3, 0.0, 36e-3+4e-3, 20e-3, 2));
+            unitCell.add(new Rect(36e-3, 44e-3, 36e-3+4e-3, 44e-3+20e-3, 2));
+            unitCell.add(new Rect(12e-3, 10e-3, 12e-3+4e-3, 10e-3+40e-3, 2));
+            unitCell.add(new Rect(24e-3, 10e-3, 24e-3+4e-3, 10e-3+40e-3, 2));
 
+            Struct array = new Struct("array") ;
+            array.add(new Array(unitCell, 0.0, 0.0, 6000, 8000, 80e-3*6000, 80e-3*8000));
+            
+            topCell.add(new Ref(array, 0, 0));
+            lib.add(new Ref(topCell, 0, 0));
+            
             lib.GDSOut(g);
             System.out.println(" Saved to " + f.getAbsolutePath());
         } catch (IOException eOutput) {
